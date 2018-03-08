83f0e2cd76c98d05705016c54a289484c2a5fb23
chris spanos <spanosc1@tcnj.edu>
2018-03-08 17:47:49 -0500

    initial commit

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,6 @@ $(window).ready(function() {
 	var currentURL = window.location.origin;
 	$.ajax({url: currentURL + "/text.json", method: "GET"})
 	.done(function(result) {
-		$("#text").HTML(result.val);
+		$("#text").html(result.val);
 	});
 });
